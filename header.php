063d971db2e944dcf9a38bb3c0411d2735deb36c
Daruuu <thedaruny16@gmail.com>
2022-10-21 23:03:58 +0200

    adding php files

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!-- Css-->
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- main navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container-fluid">
+            <!--
+            <img id="img-logo" class="img-fluid" src="/images/logo.jpg" alt="logo-website" height="96px" width="120px">
+-->
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav navbar-center">
+
+                    <!-- HOME -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="newindex.html" target="_blank">Home</a>
+                    </li>
+
+                    <!-- ABOUT -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html" target="_blank">About</a>
+                    </li>
+                    <!--FORM -->
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Form</a>
+                    </li>
+                    <!--CATEGORY -->
+                    <li class="nav-item">
+                        <a class="nav-link " href="category.html#" target="_blank">Category</a>
+                    </li>
+                    <!--target >> Opens the doc in a new window-->
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- banner-->
+    <div class="container my-5">
+
+        <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white"
+            style="background-image: url('/images/slogan.png')">
+            <h1 class="mb-3 h1"></h1>
+        </div>
+    </div>
